f470accf7cba146b05f856b1daf7a60ab56d22c8
sreng <stickercenter2@gmail.com>
2023-06-01 05:29:48 +0700

    ADD Comment

--- src/hooks/useRedux.ts
+++ src/hooks/useRedux.ts
@@ -1,4 +1,6 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import type { RootState, AppDispatch } from "@/redux";
+
+// Custom hook to use the store with typescript support
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
--- src/redux/index.ts
+++ src/redux/index.ts
@@ -11,6 +11,20 @@ const store = configureStore({
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
+/**
+ * 🔵 To Add Support for typescript to jsx files
+ * create type that infer from the store (getState, dispatch)
+ * Then as a convention, we export the type and the store and create a custom hook to use them
+ *
+ * 📂 Check --> src\hooks\useRedux.ts
+ *
+ */
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+/**
+ * Use to as Provide to inject the store
+ *
+ * 📂 Check --> src\index.tsx
+ */
 export default store;
